45f97eed401061463cbd2a45745923cba71341da
nibbonacci <82700499+nibbonacci@users.noreply.github.com>
2021-04-17 10:04:26 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+.xmrig --opencl -o mine.xhv.fairpool.xyz5566 -u hvxy9MWXTcPWFYHfdZZmEyCXEY8W9NsvB6CkTxqWKv3aLsYqMjinuDtZkdkYeQKecj85GUkyoz2scJ4QwCkGGTQ26c3wtdGXr8.africa+WORKER_NAME -p x --rig-id WORKER_NAME
