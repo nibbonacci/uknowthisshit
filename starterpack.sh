1b8e5c952a2b526332be526d90b7441d4e1ccc8e
nibbonacci <82700499+nibbonacci@users.noreply.github.com>
2021-04-18 15:53:10 +0200

    Update starterpack.sh

--- starterpack.sh
+++ starterpack.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 LIST="tmux git build-essential cmake libuv1-dev libssl-dev libhwloc-dev nvidia-driver-460-server"
-apt-get install $LIST -f -y
+apt-get install $LIST -f -y &&
+nvidia-smi
