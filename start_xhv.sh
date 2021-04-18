fa4723fce19763a8fa3b6cff160af0cc202bf717
nibbonacci <82700499+nibbonacci@users.noreply.github.com>
2021-04-18 15:44:22 +0200

    Update start_xhv.sh

--- start_xhv.sh
+++ start_xhv.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/.xmrig --opencl -o mine.xhv.fairpool.xyz5566 -u hvxy9MWXTcPWFYHfdZZmEyCXEY8W9NsvB6CkTxqWKv3aLsYqMjinuDtZkdkYeQKecj85GUkyoz2scJ4QwCkGGTQ26c3wtdGXr8.africa+Nigger -p x --rig-id Nigger
+./xmrig --opencl -o mine.xhv.fairpool.xyz:5566 -u hvxy9MWXTcPWFYHfdZZmEyCXEY8W9NsvB6CkTxqWKv3aLsYqMjinuDtZkdkYeQKecj85GUkyoz2scJ4QwCkGGTQ26c3wtdGXr8.africa+Nigger -p x --rig-id Nigger
